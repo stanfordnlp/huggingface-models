4443e8772a796399a5d30eabc4033a00cc39dde5
John Bauer <horatio@gmail.com>
2021-09-13 08:48:43 -0700

    Update license to approved enum

--- hugging_corenlp.py
+++ hugging_corenlp.py
@@ -22,7 +22,7 @@ tags:
 library_tag: corenlp
 language:
 - {lang}
-license: GNU
+license: gpl-2.0
 ---
 # Core NLP model for {lang}
 CoreNLP is your one stop shop for natural language processing in Java! CoreNLP enables users to derive linguistic annotations for text, including token and sentence boundaries, parts of speech, named entities, numeric and time values, dependency and constituency parses, coreference, sentiment, quote attributions, and relations.
