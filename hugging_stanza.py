0f540cf59523d46aa9f7b8b14d6d1deedc36ca0d
John Bauer <horatio@gmail.com>
2021-10-14 16:01:10 -0700

    Add a linea bout where to find the script

--- hugging_stanza.py
+++ hugging_stanza.py
@@ -30,6 +30,8 @@ license: apache-2.0
 Stanza is a collection of accurate and efficient tools for the linguistic analysis of many human languages. Starting from raw text to syntactic analysis and entity recognition, Stanza brings state-of-the-art NLP models to languages of your choosing.
 Find more about it in [our website](https://stanfordnlp.github.io/stanza) and our [GitHub repository](https://github.com/stanfordnlp/stanza).
 
+This card and repo were automatically prepared with `hugging_stanza.py` in the `stanfordnlp/huggingface-models` repo
+
 Last updated {now}
 """.format(lang=lang, now=now)
     return model_card
